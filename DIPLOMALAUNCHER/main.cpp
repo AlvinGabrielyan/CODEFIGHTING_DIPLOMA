411e56206256b200baa7e8db238854c1c57e7411
AlvinGabrielyan <alvin.gabrielyan2004@gmail.com>
2026-04-20 17:33:08 +0400

    Update main.cpp

--- DIPLOMALAUNCHER/main.cpp
+++ DIPLOMALAUNCHER/main.cpp
@@ -1,1 +1,12 @@
+#include "mainwindow.h"
+#include <QApplication>
 
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    a.setApplicationName("Algorithm Challenges Launcher");
+    a.setApplicationVersion("2.0");
+    MainWindow w;
+    w.show();
+    return a.exec();
+}
